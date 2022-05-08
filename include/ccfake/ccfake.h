282aced6ead678cac930528d9c0f943a28c2d47e
magicbowen <e.bowen.wang@icloud.com>
2022-05-09 07:37:44 +0800

    add header

---
+++ include/ccfake/ccfake.h
@@ -0,0 +1,8 @@
+#ifndef H46762693_3930_4E0F_B55A_E14CF9EC62CA
+#define H46762693_3930_4E0F_B55A_E14CF9EC62CA
+
+#include "ccfake/dtree/dtree.h"
+#include "ccfake/actor/actor.h"
+#include "ccfake/log/log.h"
+
+#endif
